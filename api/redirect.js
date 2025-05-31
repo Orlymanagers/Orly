0ba31c8a822e55055d6c51de7afc3aaee84b07ca
Orlymanagers <orly_k_480@hotmail.com>
2025-05-30 22:54:16 -0400

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,31 +1,24 @@
 import { Redis } from '@upstash/redis';
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: 'https://growing-teal-43802.upstash.io',
+  token: 'AasaAAIjcDE0MmUyY2U3ZWQ5Y2E0Nzg0YWVkMjYxNjM1ODEzNzgzYXAxMA',
 });
 
-const links = [
-  "https://chat.whatsapp.com/HsaBuLsrdPO4V21yjIQv47",
-  "https://chat.whatsapp.com/LgnVlowLUYT9cZae0bEI5V",
-  "https://chat.whatsapp.com/D4qSliHepxsEVmCFHm7fZK",
-  "https://chat.whatsapp.com/KFm5iHFYDgdA5RJ7LXWddZ",
-  "https://chat.whatsapp.com/Db2qc5V6ramIhAMh3eLcIC"
+const groups = [
+  'https://chat.whatsapp.com/HsaBuLsrdPO4V21yjIQv47',
+  'https://chat.whatsapp.com/LgnVlowLUYT9cZae0bEI5V',
+  'https://chat.whatsapp.com/D4qSliHepxsEVmCFHm7fZK',
+  'https://chat.whatsapp.com/KFm5iHFYDgdA5RJ7LXWddZ',
+  'https://chat.whatsapp.com/Db2qc5V6ramIhAMh3eLcIC',
 ];
 
 export default async function handler(req, res) {
-  try {
-    let clicks = await redis.get("clicks");
-    clicks = clicks ? parseInt(clicks) : 0;
-
-    const nextIndex = clicks % links.length;
-    const nextLink = links[nextIndex];
-
-    await redis.set("clicks", clicks + 1);
-
-    res.writeHead(302, { Location: nextLink });
-    res.end();
-  } catch (error) {
-    res.status(500).json({ error: "Internal Server Error", message: error.message });
-  }
+  const key = 'clicks';
+  let current = await redis.get(key);
+  if (!current) current = 0;
+  const index = current % groups.length;
+  await redis.set(key, Number(current) + 1);
+  res.writeHead(302, { Location: groups[index] });
+  res.end();
 }
